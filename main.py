8e52ba821c02d32830d2774eeea45c3743a9dec5
ft4bhi <psabhiram100@gmail.com>
2024-11-02 13:25:38 +0530

    running animation

--- main.py
+++ main.py
@@ -24,3 +24,92 @@ LARGE_Cactus = [pygame.image.load(os.path.join("Assets/Cactus","LargeCactus1.png
 
 BIRD = [pygame.image.load(os.path.join("Assets/Bird","Bird1.png")),
         pygame.image.load(os.path.join("Assets/Bird","Bird2.png"))]
+CLOUD = [pygame.image.load(os.path.join("Assets/Other","Cloud.png"))]
+BG = [pygame.image.load(os.path.join("Assets/Other","Track.png"))]
+
+
+class Dinosaur:
+    X_POS = 80
+    Y_POS = 310
+
+    def __init__(self):
+        self.duck_img = DUCKING
+        self.run_img = RUNNING
+        self.jump_img = JUMPING
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
+        
+    def update(self, userInput): 
+        if self.dino_run:
+            self.run()
+        if self.dino_jump:
+            self.jump()
+        if self.dino_duck:
+            self.duck()
+
+        if self.step_index >= 10:
+            self.step_index = 0 
+
+        if userInput[pygame.K_UP] and not self.dino_jump:
+            self.dino_duck = False
+            self.dino_jump = True
+            self.dino_run = False
+        elif userInput[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_jump = False
+            self.dino_run = False
+        elif not (self.dino_jump or userInput[pygame.K_DOWN]):
+            self.dino_duck = False
+            self.dino_run = True
+            self.dino_jump = False
+
+    def duck(self):
+        pass
+
+    def jump(self):
+        pass
+
+    def run(self):
+        self.image = self.run_img[self.step_index // 5]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+        self.step_index += 1
+        
+        
+
+    def update(self, userInput):
+        if self.dino_duck:
+            self.duck()
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+            
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    player = Dinosaur()
+    
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        SCREEN.fill((255,255,255))
+        userInput = pygame.key.get_pressed()
+
+        player.draw(SCREEN)
+        player.update(userInput)
+        clock.tick(30)
+        pygame.display.update()
+
+main()
