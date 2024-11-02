4a42ad09745569b6c239b6bc7fa4b5bcc9c4f111
ft4bhi <psabhiram100@gmail.com>
2024-11-02 13:29:20 +0530

    dino is acting properly

--- main.py
+++ main.py
@@ -2,35 +2,46 @@ import pygame
 import os
 
 pygame.init()
-#global variables
+
+# Global variables
 SCREEN_WIDTH = 1100
 SCREEN_HEIGHT = 600
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-SCREEN = pygame.display.set_mode((SCREEN_WIDTH , SCREEN_HEIGHT))
+# Load assets
+RUNNING = [
+    pygame.image.load(os.path.join("Assets/Dino", "DinoRun1.png")),
+    pygame.image.load(os.path.join("Assets/Dino", "DinoRun2.png"))
+]
+JUMPING = pygame.image.load(os.path.join("Assets/Dino", "DinoJump.png"))
+DUCKING = [
+    pygame.image.load(os.path.join("Assets/Dino", "DinoDuck1.png")),
+    pygame.image.load(os.path.join("Assets/Dino", "DinoDuck2.png"))
+]
 
-RUNNING = [pygame.image.load(os.path.join("Assets/Dino", "DinoRun1.png")),
-           pygame.image.load(os.path.join("Assets/Dino","DinoRun2.png"))]
-JUMPING = [pygame.image.load(os.path.join("Assets/Dino","DinoJump.png")),]
-DUCKING = [pygame.image.load(os.path.join("Assets/Dino","DinoDuck1.png")),
-           pygame.image.load(os.path.join("Assets/Dino","DinoDuck2.png"))]
-
-
-SMALL_Cactus = [pygame.image.load(os.path.join("Assets/Cactus","SmallCactus1.png")),
-                pygame.image.load(os.path.join("Assets/Cactus","SmallCactus2.png")),
-                pygame.image.load(os.path.join("Assets/Cactus","SmallCactus3.png"))]
-LARGE_Cactus = [pygame.image.load(os.path.join("Assets/Cactus","LargeCactus1.png")),
-                pygame.image.load(os.path.join("Assets/Cactus","LargeCactus2.png")),
-                pygame.image.load(os.path.join("Assets/Cactus","LargeCactus3.png"))]
-
-BIRD = [pygame.image.load(os.path.join("Assets/Bird","Bird1.png")),
-        pygame.image.load(os.path.join("Assets/Bird","Bird2.png"))]
-CLOUD = [pygame.image.load(os.path.join("Assets/Other","Cloud.png"))]
-BG = [pygame.image.load(os.path.join("Assets/Other","Track.png"))]
-
+# Cactus and other assets
+SMALL_CACTUS = [
+    pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus1.png")),
+    pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus2.png")),
+    pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus3.png"))
+]
+LARGE_CACTUS = [
+    pygame.image.load(os.path.join("Assets/Cactus", "LargeCactus1.png")),
+    pygame.image.load(os.path.join("Assets/Cactus", "LargeCactus2.png")),
+    pygame.image.load(os.path.join("Assets/Cactus", "LargeCactus3.png"))
+]
+BIRD = [
+    pygame.image.load(os.path.join("Assets/Bird", "Bird1.png")),
+    pygame.image.load(os.path.join("Assets/Bird", "Bird2.png"))
+]
+CLOUD = pygame.image.load(os.path.join("Assets/Other", "Cloud.png"))
+BG = pygame.image.load(os.path.join("Assets/Other", "Track.png"))
 
 class Dinosaur:
     X_POS = 80
     Y_POS = 310
+    Y_POS_DUCK = 340
+    JUMP_VEL = 8.5
 
     def __init__(self):
         self.duck_img = DUCKING
@@ -42,41 +53,55 @@ class Dinosaur:
         self.dino_jump = False
 
         self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
         self.image = self.run_img[0]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_POS
         self.dino_rect.y = self.Y_POS
 
-        
-    def update(self, userInput): 
-        if self.dino_run:
-            self.run()
-        if self.dino_jump:
-            self.jump()
-        if self.dino_duck:
-            self.duck()
-
-        if self.step_index >= 10:
-            self.step_index = 0 
-
-        if userInput[pygame.K_UP] and not self.dino_jump:
+    def update(self, userInput):
+        # Determine state based on input
+        if userInput[pygame.K_UP] and not self.dino_jump:  # Jump
             self.dino_duck = False
             self.dino_jump = True
             self.dino_run = False
-        elif userInput[pygame.K_DOWN] and not self.dino_jump:
+        elif userInput[pygame.K_DOWN] and not self.dino_jump:  # Duck
             self.dino_duck = True
             self.dino_jump = False
             self.dino_run = False
-        elif not (self.dino_jump or userInput[pygame.K_DOWN]):
+        elif not (self.dino_jump or userInput[pygame.K_DOWN]):  # Run by default
             self.dino_duck = False
             self.dino_run = True
             self.dino_jump = False
 
+        # Execute state-specific action
+        if self.dino_duck:
+            self.duck()
+        elif self.dino_jump:
+            self.jump()
+        elif self.dino_run:
+            self.run()
+
+        # Reset the step index for animation cycle
+        if self.step_index >= 10:
+            self.step_index = 0
+
     def duck(self):
-        pass
+        self.image = self.duck_img[self.step_index // 5]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS_DUCK
+        self.step_index += 1
 
     def jump(self):
-        pass
+        self.image = self.jump_img
+        if self.dino_jump:
+            self.dino_rect.y -= self.jump_vel * 4
+            self.jump_vel -= 0.8  # Gravity effect
+        if self.jump_vel < -self.JUMP_VEL:
+            self.dino_jump = False
+            self.jump_vel = self.JUMP_VEL  
+            self.dino_rect.y = self.Y_POS  
 
     def run(self):
         self.image = self.run_img[self.step_index // 5]
@@ -84,12 +109,6 @@ class Dinosaur:
         self.dino_rect.x = self.X_POS
         self.dino_rect.y = self.Y_POS
         self.step_index += 1
-        
-        
-
-    def update(self, userInput):
-        if self.dino_duck:
-            self.duck()
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
@@ -104,11 +123,11 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        SCREEN.fill((255,255,255))
+        SCREEN.fill((255, 255, 255))
         userInput = pygame.key.get_pressed()
 
-        player.draw(SCREEN)
-        player.update(userInput)
+        player.update(userInput) 
+        player.draw(SCREEN)  
         clock.tick(30)
         pygame.display.update()
 
